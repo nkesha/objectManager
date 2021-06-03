f41f4033258e2ae405a7e8805d2990fdccb2858c
nkesha <nkeshice@myumanitoba.ca>
2021-06-03 03:40:13 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,13 +1,6 @@
-//-----------------------------------------
+
 // NAME: chris Nkeshimana 
-// STUDENT NUMBER: 7735006
-// COURSE: COMP 2160
-// INSTRUCTOR: Michael Zapp
-// ASSIGNMENT: Assignment 4
-// 
-// REMARKS: Object Manager
-//
-//-----------------------------------------
+
 
 
 
